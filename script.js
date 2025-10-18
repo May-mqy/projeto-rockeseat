4477f6c61e45cc9f530657d1ad088f32b117dbda
May-mqy <mayar_234@outlook.com>
2025-10-18 03:01:43 -0300

    finalizado com imagem

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
-
-  if(html.classList.contains('light')) {
-    html.classList.remove('light')
-  } else {
-    html.classList.add('light')
+  html.classList.toggle('light') //Esse código tem a mesma função do if else
+  
+  //pegar a tag img
+    const img = document.querySelector("#profile img")
+  //substituir a imagem
+    if(html.classList.contains('light')) {
+  //Se tiver Ligth mode, adicionar a imagem light
+      img.setAttribute("src", "./imagens/avatar May-light.png")
+      img.setAttribute("alt", "Imagem da Mayara sorrindo no carro, cabelos cacheados e cropped roxo."  )
+    } else {
+ //se tiver sem Light mode, manter a imagem normal
+      img.setAttribute("src", "./imagens/avatar May-dark.png")
+      img.setAttribute("alt", "Foto de Mayara sorrindo sem mostrar os dentes, com cabelos soltos cacheados.")
   }
 }
